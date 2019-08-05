76b2d46ee251858180266891dea9ff79f1c32f1a
Steven Hobson-Campbell <xapphire13@live.com>
2019-08-05 00:00:48 -0700

    Restructure code

---
+++ src/renderer/FolderVisualization.tsx
@@ -0,0 +1,218 @@
+import React from 'react';
+import * as PIXI from 'pixi.js';
+import path from 'path';
+import PixiCanvas, { OnStageReadyParams } from './pixi-canvas';
+
+import { KondoDirectory, KondoType } from '../test';
+
+const MIN_WIDTH = Math.PI / 180;
+
+function getColor(type: KondoType) {
+  switch (+type) {
+    case KondoType.Audio:
+      return 0xff0000;
+    case KondoType.Document:
+      return 0x00ff00;
+    case KondoType.Image:
+      return 0x0000ff;
+    case KondoType.SourceCode:
+      return 0x0ff000;
+    case KondoType.Video:
+      return 0x000ff0;
+    case KondoType.ApplicationFile:
+      return 0xf0f000;
+    case KondoType.Archive:
+      return 0x00f0f0;
+    case KondoType.Other:
+    default:
+      return 0x777777;
+  }
+}
+
+async function onRender({
+  height,
+  stage,
+  width,
+  directory,
+  onDirectorySelected,
+  dirPath
+}: OnStageReadyParams & FolderVisualizationProps) {
+  const centerX = width / 2;
+  const centerY = height / 2;
+
+  renderDirectory({
+    centerX,
+    centerY,
+    directory,
+    minAngle: 0,
+    maxAngle: 2 * Math.PI,
+    shellNumber: 1,
+    stage,
+    dirPath,
+    onSelect: onDirectorySelected
+  });
+}
+
+function renderDirectory({
+  directory,
+  centerX,
+  centerY,
+  minAngle,
+  maxAngle,
+  shellNumber,
+  stage,
+  dirPath,
+  onSelect
+}: {
+  directory: KondoDirectory;
+  centerX: number;
+  centerY: number;
+  minAngle: number;
+  maxAngle: number;
+  shellNumber: number;
+  stage: PIXI.Container;
+  dirPath: string;
+  onSelect: (dir: KondoDirectory, dirPath: string) => void;
+}) {
+  let startAngle = minAngle;
+  const angleWidth = maxAngle - minAngle;
+  let smallFilesSize = 0;
+
+  // Render directories
+  for (const childDir of directory.children) {
+    if (childDir.size === 0) continue;
+
+    const percentage = childDir.size / directory.size;
+    const endAngle = startAngle + angleWidth * percentage;
+    const childWidth = endAngle - startAngle;
+
+    if (childWidth >= MIN_WIDTH) {
+      const arc = createArc({
+        centerX,
+        centerY,
+        innerRadius: 10 + shellNumber * 25,
+        size: 25,
+        startAngle,
+        endAngle,
+        color: getColor(childDir.type)
+      });
+
+      arc.interactive = true;
+      arc.addListener('pointertap', () =>
+        onSelect(childDir, path.join(dirPath, childDir.path))
+      );
+      stage.addChild(arc);
+
+      renderDirectory({
+        centerX,
+        centerY,
+        directory: childDir,
+        minAngle: startAngle,
+        maxAngle: endAngle,
+        shellNumber: shellNumber + 1,
+        stage,
+        dirPath: path.join(dirPath, childDir.path),
+        onSelect
+      });
+
+      startAngle = endAngle;
+    } else {
+      smallFilesSize += childDir.size;
+    }
+  }
+
+  // Render other files
+  Object.keys(KondoType)
+    // eslint-disable-next-line no-restricted-globals
+    .filter((key: any) => !isNaN(key))
+    .map(Number)
+    .forEach((type: KondoType) => {
+      const filesPercentage =
+        directory.childrenFileSizes[type] / directory.size;
+      const filesEndAngle = startAngle + angleWidth * filesPercentage;
+      const filesArc = createArc({
+        centerX,
+        centerY,
+        innerRadius: 10 + shellNumber * 25,
+        size: 5,
+        startAngle,
+        endAngle: filesEndAngle,
+        color: getColor(type)
+      });
+
+      stage.addChild(filesArc);
+      startAngle = filesEndAngle;
+    });
+
+  // Render small files
+  const smallFilesPercentage = smallFilesSize / directory.size;
+  const smallFilesEndAngle = startAngle + angleWidth * smallFilesPercentage;
+  const smallFilesArc = createArc({
+    centerX,
+    centerY,
+    innerRadius: 10 + shellNumber * 25,
+    size: 5,
+    startAngle,
+    endAngle: smallFilesEndAngle,
+    color: 0x444444
+  });
+
+  stage.addChild(smallFilesArc);
+}
+
+function createArc({
+  centerX,
+  centerY,
+  innerRadius,
+  size,
+  startAngle,
+  endAngle,
+  color
+}: {
+  centerX: number;
+  centerY: number;
+  innerRadius: number;
+  size: number;
+  startAngle: number;
+  endAngle: number;
+  color: number;
+}) {
+  const outerRadius = innerRadius + size;
+  const borderThickness = 0.5;
+  const calcX = (radius: number, angle: number) =>
+    centerX + radius * Math.cos(angle);
+  const calcY = (radius: number, angle: number) =>
+    centerY + radius * Math.sin(angle);
+
+  return new PIXI.Graphics()
+    .lineStyle(borderThickness, 0x000000)
+    .moveTo(calcX(innerRadius, startAngle), calcY(innerRadius, startAngle))
+    .beginFill(color)
+    .lineTo(calcX(outerRadius, startAngle), calcY(outerRadius, startAngle))
+    .arc(centerX, centerY, outerRadius, startAngle, endAngle)
+    .lineTo(calcX(innerRadius, endAngle), calcY(innerRadius, endAngle))
+    .arc(centerX, centerY, innerRadius, endAngle, startAngle, true)
+    .endFill();
+}
+
+export type FolderVisualizationProps = {
+  directory: KondoDirectory;
+  onDirectorySelected: (directory: KondoDirectory) => void;
+  dirPath: string;
+};
+
+export default function FolderVisualization({
+  directory,
+  onDirectorySelected,
+  dirPath
+}: FolderVisualizationProps) {
+  const onStageReady = (params: OnStageReadyParams) =>
+    onRender({
+      ...params,
+      directory,
+      onDirectorySelected,
+      dirPath
+    });
+
+  return <PixiCanvas onStageReady={onStageReady} />;
+}
--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,248 +1,57 @@
-import React, { useState } from 'react';
-import { ipcRenderer } from 'electron'; // eslint-disable-line
-import * as PIXI from 'pixi.js';
-import fs from 'fs';
+import React, { useState, useEffect } from 'react';
 import path from 'path';
+import { ipcRenderer } from 'electron'; // eslint-disable-line
+import fs from 'fs';
 import { promisify } from 'util';
-import PixiCanvas, { OnStageReadyParams } from './pixi-canvas';
-import { KondoDirectory, KondoType } from '../test';
+import { KondoDirectory } from '../test';
+import FolderVisualization from './FolderVisualization';
 
 const readFile = promisify(fs.readFile);
-const MIN_WIDTH = Math.PI / 180;
 const generateNew = false;
-
-function getColor(type: KondoType) {
-  switch (+type) {
-    case KondoType.Audio:
-      return 0xff0000;
-    case KondoType.Document:
-      return 0x00ff00;
-    case KondoType.Image:
-      return 0x0000ff;
-    case KondoType.SourceCode:
-      return 0x0ff000;
-    case KondoType.Video:
-      return 0x000ff0;
-    case KondoType.ApplicationFile:
-      return 0xf0f000;
-    case KondoType.Archive:
-      return 0x00f0f0;
-    case KondoType.Other:
-    default:
-      return 0x777777;
-  }
-}
 
 export default function App() {
   const [selectedDir, setSelectedDir] = useState<KondoDirectory>();
-  const [selectedDirPath, setSelectedDirPath] = useState<string>('/');
+  const [dirPath, setDirPath] = useState<string>('/');
 
-  const onStageReady = (params: OnStageReadyParams) =>
-    onRender({
-      ...params,
-      selectedDir,
-      setSelectedDir,
-      selectedDirPath,
-      setSelectedDirPath
-    });
+  const onDirectorySelected = (dir: KondoDirectory) => {
+    setSelectedDir(dir);
+    setDirPath(path.join(dirPath, dir.path));
+  };
+
+  useEffect(() => {
+    async function LoadScanResults() {
+      let cachedPath = path.join(process.env.HOME || '', 'kondo-backup');
+
+      if (generateNew || !cachedPath) {
+        const scanResultsPath = new Promise<string>(res => {
+          ipcRenderer.once('scan-complete', (_ev: any, args: any) => res(args));
+        });
+        ipcRenderer.send('scan-disk');
+
+        cachedPath = await scanResultsPath;
+      }
+
+      const rootDirectory: KondoDirectory = JSON.parse(
+        await readFile(cachedPath, { encoding: 'utf8' })
+      );
+
+      setSelectedDir(rootDirectory);
+    }
+
+    if (!selectedDir) {
+      LoadScanResults();
+    }
+  });
 
   return (
     <div style={{ background: '#21252B', width: '100%', height: '100%' }}>
-      <PixiCanvas onStageReady={onStageReady} />
+      {selectedDir && (
+        <FolderVisualization
+          directory={selectedDir}
+          onDirectorySelected={onDirectorySelected}
+          dirPath={dirPath}
+        />
+      )}
     </div>
   );
-}
-
-async function onRender({
-  height,
-  stage,
-  width,
-  selectedDir,
-  setSelectedDir,
-  selectedDirPath,
-  setSelectedDirPath
-}: OnStageReadyParams & {
-  selectedDir?: KondoDirectory;
-  setSelectedDir: (dir: KondoDirectory) => void;
-  selectedDirPath: string;
-  setSelectedDirPath: (path: string) => void;
-}) {
-  if (!selectedDir) {
-    let cachedPath = path.join(process.env.HOME || '', 'kondo-backup');
-
-    if (generateNew || !cachedPath) {
-      const scanResultsPath = new Promise<string>(res => {
-        ipcRenderer.once('scan-complete', (_ev: any, args: any) => res(args));
-      });
-      ipcRenderer.send('scan-disk');
-
-      cachedPath = await scanResultsPath;
-    }
-
-    const rootDirectory: KondoDirectory = JSON.parse(
-      await readFile(cachedPath, { encoding: 'utf8' })
-    );
-
-    setSelectedDir(rootDirectory);
-  } else {
-    const centerX = width / 2;
-    const centerY = height / 2;
-
-    renderDirectory({
-      centerX,
-      centerY,
-      directory: selectedDir,
-      minAngle: 0,
-      maxAngle: 2 * Math.PI,
-      shellNumber: 1,
-      stage,
-      dirPath: selectedDirPath,
-      onSelect: (dir, dirPath) => {
-        setSelectedDir(dir);
-        setSelectedDirPath(dirPath);
-      }
-    });
-  }
-}
-
-function renderDirectory({
-  directory,
-  centerX,
-  centerY,
-  minAngle,
-  maxAngle,
-  shellNumber,
-  stage,
-  dirPath,
-  onSelect
-}: {
-  directory: KondoDirectory;
-  centerX: number;
-  centerY: number;
-  minAngle: number;
-  maxAngle: number;
-  shellNumber: number;
-  stage: PIXI.Container;
-  dirPath: string;
-  onSelect: (dir: KondoDirectory, dirPath: string) => void;
-}) {
-  let startAngle = minAngle;
-  const angleWidth = maxAngle - minAngle;
-  let smallFilesSize = 0;
-
-  // Render directories
-  for (const childDir of directory.children) {
-    if (childDir.size === 0) continue;
-
-    const percentage = childDir.size / directory.size;
-    const endAngle = startAngle + angleWidth * percentage;
-    const childWidth = endAngle - startAngle;
-
-    if (childWidth >= MIN_WIDTH) {
-      const arc = createArc({
-        centerX,
-        centerY,
-        innerRadius: 10 + shellNumber * 25,
-        size: 25,
-        startAngle,
-        endAngle,
-        color: getColor(childDir.type)
-      });
-
-      arc.interactive = true;
-      arc.addListener('pointertap', () =>
-        onSelect(childDir, path.join(dirPath, childDir.path))
-      );
-      stage.addChild(arc);
-
-      renderDirectory({
-        centerX,
-        centerY,
-        directory: childDir,
-        minAngle: startAngle,
-        maxAngle: endAngle,
-        shellNumber: shellNumber + 1,
-        stage,
-        dirPath: path.join(dirPath, childDir.path),
-        onSelect
-      });
-
-      startAngle = endAngle;
-    } else {
-      smallFilesSize += childDir.size;
-    }
-  }
-
-  // Render other files
-  Object.keys(KondoType)
-    // eslint-disable-next-line no-restricted-globals
-    .filter((key: any) => !isNaN(key))
-    .map(Number)
-    .forEach((type: KondoType) => {
-      const filesPercentage =
-        directory.childrenFileSizes[type] / directory.size;
-      const filesEndAngle = startAngle + angleWidth * filesPercentage;
-      const filesArc = createArc({
-        centerX,
-        centerY,
-        innerRadius: 10 + shellNumber * 25,
-        size: 5,
-        startAngle,
-        endAngle: filesEndAngle,
-        color: getColor(type)
-      });
-
-      stage.addChild(filesArc);
-      startAngle = filesEndAngle;
-    });
-
-  // Render small files
-  const smallFilesPercentage = smallFilesSize / directory.size;
-  const smallFilesEndAngle = startAngle + angleWidth * smallFilesPercentage;
-  const smallFilesArc = createArc({
-    centerX,
-    centerY,
-    innerRadius: 10 + shellNumber * 25,
-    size: 5,
-    startAngle,
-    endAngle: smallFilesEndAngle,
-    color: 0x444444
-  });
-
-  stage.addChild(smallFilesArc);
-}
-
-function createArc({
-  centerX,
-  centerY,
-  innerRadius,
-  size,
-  startAngle,
-  endAngle,
-  color
-}: {
-  centerX: number;
-  centerY: number;
-  innerRadius: number;
-  size: number;
-  startAngle: number;
-  endAngle: number;
-  color: number;
-}) {
-  const outerRadius = innerRadius + size;
-  const borderThickness = 0.5;
-  const calcX = (radius: number, angle: number) =>
-    centerX + radius * Math.cos(angle);
-  const calcY = (radius: number, angle: number) =>
-    centerY + radius * Math.sin(angle);
-
-  return new PIXI.Graphics()
-    .lineStyle(borderThickness, 0x000000)
-    .moveTo(calcX(innerRadius, startAngle), calcY(innerRadius, startAngle))
-    .beginFill(color)
-    .lineTo(calcX(outerRadius, startAngle), calcY(outerRadius, startAngle))
-    .arc(centerX, centerY, outerRadius, startAngle, endAngle)
-    .lineTo(calcX(innerRadius, endAngle), calcY(innerRadius, endAngle))
-    .arc(centerX, centerY, innerRadius, endAngle, startAngle, true)
-    .endFill();
 }

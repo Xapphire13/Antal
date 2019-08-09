6101040466a155638f0df6bc651c881e4e1c4776
Steven Hobson-Campbell <xapphire13@live.com>
2019-08-08 21:51:53 -0700

    Flatten eslint plugin deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,28 @@
-const ReactWithStyles = require("eslint-plugin-react-with-styles/lib/index");
-
 module.exports = {
-  plugins: ["prettier", "react-with-styles", "react-hooks"],
-  extends: ["airbnb-typescript", "plugin:prettier/recommended", "prettier/react"],
+  plugins: ["@typescript-eslint", "prettier", "react-with-styles", "react-hooks"],
+  extends: ["airbnb", "prettier", "prettier/react", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
+  settings: {
+    // Append 'ts' and 'tsx' extensions to Airbnb 'import/resolver' setting
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".ts", ".mjs", ".jsx", ".tsx", ".json"],
+      },
+    },
+    // Append 'ts' and 'tsx' extensions to Airbnb 'import/extensions' setting
+    "import/extensions": [".js", ".ts", ".mjs", ".jsx", ".tsx"],
+  },
   env: { browser: true },
   rules: {
-    ...ReactWithStyles.configs.recommended.rules,
+    'react-with-styles/only-spread-css': 'error',
+    'react-with-styles/no-unused-styles': 'error',
+    'no-restricted-imports': ['error', {
+      paths: [{
+        name: 'react-with-styles',
+        importNames: ['css', 'cssNoRTL'],
+        message: 'The global `css` and `cssNoRTL` exports are deprecated. Please use `this.props.css` instead!',
+      }],
+    }],
     "@typescript-eslint/indent": "off",
     "no-continue": "off",
     "no-param-reassign": "warn",
@@ -15,5 +32,85 @@ module.exports = {
     "prettier/prettier": ["error", { singleQuote: true }],
     "react-hooks/exhaustive-deps": "error",
     "react-hooks/rules-of-hooks": "error",
+    "react/jsx-filename-extension": ["error", { extensions: [".jsx", ".tsx"] }],
+    "arrow-parens": [
+      "error",
+      "as-needed",
+      {
+        // `requireForBlockBody` should be `true` (as Airbnb sets this to `true`), however it causes problems with HOC syntax in TS
+        // https://github.com/iamturns/eslint-config-airbnb-typescript/issues/8
+        requireForBlockBody: false,
+      },
+    ],
+
+    // Replace Airbnb 'camelcase' rule with '@typescript-eslint' version
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/camelcase.md
+    camelcase: "off",
+    "@typescript-eslint/camelcase": ["error", { properties: "never" }],
+
+    // Replace Airbnb 'no-array-constructor' rule with '@typescript-eslint' version
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-array-constructor.md
+    "no-array-constructor": "off",
+    "@typescript-eslint/no-array-constructor": "error",
+
+    // Replace Airbnb 'no-unused-vars' rule with '@typescript-eslint' version
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unused-vars.md
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { vars: "all", args: "after-used", ignoreRestSiblings: true },
+    ],
+
+    // Append `ts` and `tsx` extensions to Airbnb 'import/no-extraneous-dependencies' rule
+    // Forbid the use of extraneous packages
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-extraneous-dependencies.md
+    // Paths are treated both as absolute paths, and relative to process.cwd()
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: [
+          "test/**", // tape, common npm pattern
+          "tests/**", // also common npm pattern
+          "spec/**", // mocha, rspec-like pattern
+          "**/__tests__/**", // jest pattern
+          "**/__mocks__/**", // jest pattern
+          "test.{js,jsx,ts,tsx}", // repos with a single test file
+          "test-*.{js,jsx,ts,tsx}", // repos with multiple top-level test files
+          "**/*{.,_}{test,spec}.{js,jsx,ts,tsx}", // tests where the extension or filename suffix denotes that it is a test
+          "**/jest.config.js", // jest config
+          "**/vue.config.js", // vue-cli config
+          "**/webpack.config.js", // webpack config
+          "**/webpack.config.*.js", // webpack config
+          "**/rollup.config.js", // rollup config
+          "**/rollup.config.*.js", // rollup config
+          "**/gulpfile.js", // gulp config
+          "**/gulpfile.*.js", // gulp config
+          "**/Gruntfile{,.js}", // grunt config
+          "**/protractor.conf.js", // protractor config
+          "**/protractor.conf.*.js", // protractor config
+        ],
+        optionalDependencies: false,
+      },
+    ],
+
+    // Append `ts` and `tsx` to Airbnb 'import/extensions' rule
+    // Ensure consistent use of file extension within the import path
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        mjs: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+
+    // Replace Airbnb 'semi' rule with '@typescript-eslint' version
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/semi.md
+    semi: "off",
+    "@typescript-eslint/semi": ["error", "always"],
   }
 }
